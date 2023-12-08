de1cf8d104ace82f65ef728cd231fc9c1a4ec0c6
Seong Minsik <122091776+minvamos@users.noreply.github.com>
2023-12-08 17:50:18 +0900

    train
    you need to change the Absolute path 
    my PC doesn't work with relative path now, I'll fix it

--- train.py
+++ train.py
@@ -1,76 +1,89 @@
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import DataLoader
-from torchvision import datasets, transforms
-from torchvision import models  # models 모듈 추가
+# need to change it to Annotation YOLO only.
+# need to change it to Annotation YOLO only.
+# need to change it to Annotation YOLO only.
+# need to change it to Annotation YOLO only.
+# need to change it to Annotation YOLO only.
+# need to change it to Annotation YOLO only.
+# need to change it to Annotation YOLO only.
+# need to change it to Annotation YOLO only.
+# need to change it to Annotation YOLO only.
+# need to change it to Annotation YOLO only.
 
-# Define the custom VGG19 model
-class CustomVGG19(nn.Module):
-    def __init__(self, num_classes=2):
-        super(CustomVGG19, self).__init__()
-        
-        # Load pre-trained VGG19 model
-        vgg19_model = models.vgg19(pretrained=True)
-        
-        # Extract feature layers (excluding fully connected layers)
-        self.features = vgg19_model.features
-        
-        # Add a custom fully connected layer for classification
-        self.classifier = nn.Sequential(
-            nn.Linear(512 * 7 * 7, 4096),
-            nn.ReLU(True),
-            nn.Dropout(),
-            nn.Linear(4096, 4096),
-            nn.ReLU(True),
-            nn.Dropout(),
-            nn.Linear(4096, num_classes)
-        )
+import numpy as np
+import pandas as pd
+import cv2
+from sklearn.model_selection import train_test_split
+from yolov4.tf import YOLOv4, YOLODataset
+from tensorflow.keras.callbacks import Callback
 
-    def forward(self, x):
-        x = self.features(x)
-        x = x.view(x.size(0), -1)
-        x = self.classifier(x)
-        return x
+count = 1
+count2 = 1
 
-# Set the device (GPU if available, otherwise CPU)
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+# CSV 파일에서 데이터 읽어오기
+csv_path = "/Users/minsik/development/Blind_Helper/Annotations/training_file.csv"
+df = pd.read_csv(csv_path)
 
-# Data preprocessing and DataLoader setup
-transform = transforms.Compose([
-    transforms.Resize((224, 224)),  # VGG19-compatible image size
-    transforms.ToTensor(),
-])
+# Label이 0 또는 1인 데이터만 선택
+df = df[df['mode'].isin([0, 1])]
 
-# Load the dataset
-train_dataset = datasets.ImageFolder(root='dataset', transform=transform)
+# 처음부터 1000개의 데이터만 사용
+df = df.head(200)
 
-# DataLoader setup
-train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers=4)
+# 이미지 경로, 박스 좌표 및 라벨 추출
+image_paths = df['file'].values
+labels = df['mode'].values
+boxes = df[['x1', 'y1', 'x2', 'y2']].values
 
-# Initialize the model
-model = CustomVGG19(num_classes=2).to(device)
+# 이미지 리사이즈 및 박스 좌표 조정 함수
+def resize_and_adjust_boxes(image_path, box, label, resize_factor):
+    global count  # 전역 변수로 선언
+    img = cv2.imread("/Users/minsik/development/Blind_Helper/Img/" + image_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-# Loss function and optimizer setup
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001)
+    # 이미지 크기를 4분의 1로 줄임
+    new_size = (img.shape[1] // resize_factor, img.shape[0] // resize_factor)
+    img = cv2.resize(img, new_size)
 
-# Training loop
-num_epochs = 10
+    # 박스 좌표 조정
+    box = [coord // resize_factor for coord in box]
 
-for epoch in range(num_epochs):
-    model.train()
-    for batch in train_loader:
-        inputs, targets = batch
-        inputs, targets = inputs.to(device), targets.to(device)
+    # 이미지를 0과 1 사이의 값으로 정규화
+    img = img / 255.0
+    print("Resized " + str(count) + " Imgs")
+    count += 1  # global 키워드 사용하여 값을 증가시킴
+    return img, box, label
 
-        optimizer.zero_grad()
-        outputs = model(inputs)
-        loss = criterion(outputs, targets)
-        loss.backward()
-        optimizer.step()
+# 데이터셋 생성
+images = []
+adjusted_boxes = []
+for path, box, label in zip(image_paths, boxes, labels):
+    img, adjusted_box, label = resize_and_adjust_boxes(path, box, label, 4)
+    images.append(img)
+    adjusted_boxes.append(adjusted_box)
+    print(str(count2) + " data created")
+    count2 += 1
 
-    print(f'Epoch {epoch+1}/{num_epochs}, Loss: {loss.item()}')
+images = np.array(images)
+adjusted_boxes = np.array(adjusted_boxes)
+labels = np.array(labels)
 
-# Save the model
-torch.save(model.state_dict(), 'custom_vgg19_model.pth')
+# 학습 및 검증 데이터로 분리
+X_train, X_val, y_train, y_val, boxes_train, boxes_val = train_test_split(images, labels, adjusted_boxes, test_size=0.2, random_state=42)
+
+# YOLOv4 모델 불러오기
+yolo = YOLOv4()
+
+# 데이터셋 생성
+train_dataset = YOLODataset(X_train, boxes_train, labels)
+val_dataset = YOLODataset(X_val, boxes_val, y_val)
+
+# 콜백 정의: 학습 중에 손실과 정확도를 출력
+class PrintLossAndAccuracy(Callback):
+    def on_epoch_end(self, epoch, logs=None):
+        print(f"\nEpoch {epoch + 1}/{self.params['epochs']} - loss: {logs['yolo_output_loss']} - accuracy: {logs['yolo_output_accuracy']} - val_loss: {logs['val_yolo_output_loss']} - val_accuracy: {logs['val_yolo_output_accuracy']}")
+
+# 모델 학습
+yolo.fit(train_dataset, epochs=10, validation_data=val_dataset, callbacks=[PrintLossAndAccuracy()])
+
+# 예측을 수행할 때는 yolo.predict 대신에 model.predict를 사용합니다.
+yolo.predict(X_val)
